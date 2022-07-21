9b0c5544619560c870f193634f8338e8deb0e832
miotomita <mio.tomita@nex.nikkei.com>
2022-07-21 11:54:44 -0400

    update data

--- areachart3.js
+++ areachart3.js
@@ -4,7 +4,7 @@ const margin = {top: 60, right: 230, bottom: 50, left: 50},
     height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-const svg = d3.select("#my_dataviz2")
+const svg = d3.select("#my_dataviz3")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
